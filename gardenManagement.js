ed53770052628df1c16c5bfee77e2e2aa6b61959
Alex Kipkorir <alexkipkorir909@gmail.com>
2026-09-25 00:39:46 +0300

    addd javaScript

--- gardenManagement.js
+++ gardenManagement.js
@@ -2,19 +2,22 @@ const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
 
-if (temperature >80){
-    console.log("watering on")
-}else{
-    console.log("watering off")
+// 1. Watering Control
+if (temperature > 80) {
+console.log("Watering on");
+} else {
+console.log("Watering off");
 }
 
-if (timeOfDay evining||night){
-    console.log("lights on")
-}else{
-    console.log("lights off")
+// 2. Garden Lights Control
+if (timeOfDay === "evening" || timeOfDay === "night") {
+console.log("Lights on");
+} else {
+console.log("Lights off");
 }
 
-while (soilMoisture = 40){
-    console.log(40)
-    soilMoisture+5
+// 3. Soil Moisture Adjustment
+while (soilMoisture < 40) {
+soilMoisture += 5;
+console.log(soilMoisture);
 }
